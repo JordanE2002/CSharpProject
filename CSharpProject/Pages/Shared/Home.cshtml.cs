b10fc92e7c93eef3c029eff749f05ae279431cdd
Jordan English <jenglish717@gmail.com>
2025-07-02 16:44:05 +0100

    Fixed home page

--- CSharpProject/Pages/Shared/Home.cshtml.cs
+++ CSharpProject/Pages/Shared/Home.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using CSharpProject.Data;
 using CSharpProject.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSharpProject.Pages
@@ -16,12 +17,22 @@ namespace CSharpProject.Pages
             _context = context;
         }
 
-        public List<Company> Companies { get; set; } = new();
+        public List<Company> LatestCompanies { get; set; } = new();
+        public List<Employee> LatestEmployees { get; set; } = new();
 
         public async Task OnGetAsync()
         {
-            Companies = await _context.Companies
-                .Include(c => c.Employees)
+            LatestCompanies = await _context.Companies
+                .OrderByDescending(c => c.Id)  // Or CreatedDate if you have it
+                .Take(5)
+                .AsNoTracking()
+                .ToListAsync();
+
+            LatestEmployees = await _context.Employees
+                .Include(e => e.Company)
+                .OrderByDescending(e => e.Id)  // Or CreatedDate if available
+                .Take(5)
+                .AsNoTracking()
                 .ToListAsync();
         }
     }
